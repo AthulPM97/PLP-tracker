b5ab01dc0c0e0fae7c5d47839bb7fee0084b1b45
athulpm <97athulpm@gmail.com>
2023-02-27 13:20:43 +0530

    Initial setup

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.plptracker.app',
+  appName: 'PLP tracker',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { setupIonicReact } from "@ionic/react";
+import './theme/variables.css';
+
+/* Core CSS required for Ionic components to work properly */
+import "@ionic/react/css/core.css";
+
+setupIonicReact();
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+    </div>
+  );
+}
+
+export default App;
